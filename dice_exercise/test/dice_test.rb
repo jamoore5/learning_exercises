f03c87cfd610f1306503806f3b602adecbc0e7f3
Julie Anne Moore <jmoore5@unb.ca>
2019-10-16 19:27:02 -0300

    working code

--- dice_exercise/test/dice_test.rb
+++ dice_exercise/test/dice_test.rb
@@ -10,5 +10,11 @@ class DiceTest < Minitest::Test
       result = dice.roll
       assert result.is_a?(Integer)
     end
+
+    should 'return a result between 1 and 6' do
+      dice = Dice.new
+      result = dice.roll
+      assert [1,2,3,4,5,6].include?(result)
+    end
   end
 end
