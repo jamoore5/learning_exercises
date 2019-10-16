847d12a69425a4f87e60f9ca97136c15eec90371
Julie Anne Moore <jmoore5@unb.ca>
2019-10-16 20:55:28 -0300

    working code

--- dice_exercise/test/dice_rolling_simulator_test.rb
+++ dice_exercise/test/dice_rolling_simulator_test.rb
@@ -10,10 +10,8 @@ require './lib/dice_rolling_simulator.rb'
 class DiceRollingSimulatorTest < Minitest::Test
   context '#run' do
     should 'ask the user if they want to continue' do
-      console_interface = ConsoleInterface.new
+      console_interface = mock_console_interface
       console_interface.expects(:ask_question)
-      console_interface.stubs(:answer).returns(false)
-      console_interface.stubs(:print_result)
       
       simulator = DiceRollingSimulator.new(console_interface: console_interface)
       simulator.run
@@ -48,11 +46,19 @@ class DiceRollingSimulatorTest < Minitest::Test
 
       console_interface = ConsoleInterface.new
       console_interface.expects(:print_result).with("You rolled a 5.").times(3)
-      console_interface.stubs(:ask_question).times(3)
+      console_interface.expects(:ask_question).times(3)
       console_interface.stubs(:answer).returns(true).returns(true).then.returns(false)
 
       simulator = DiceRollingSimulator.new(dice: dice, console_interface: console_interface)
       simulator.run
     end
+  end
+
+  def mock_console_interface
+    console_interface = ConsoleInterface.new
+    console_interface.stubs(:print_result)
+    console_interface.stubs(:ask_question)
+    console_interface.stubs(:answer).returns(false)
+    console_interface
   end
 end
