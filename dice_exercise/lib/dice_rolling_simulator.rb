68f96bd4089d5cd5045161b206f4b009329875e7
Julie Anne Moore <jmoore5@unb.ca>
2019-10-16 20:28:28 -0300

    working code

--- dice_exercise/lib/dice_rolling_simulator.rb
+++ dice_exercise/lib/dice_rolling_simulator.rb
@@ -1,13 +1,15 @@
 class DiceRollingSimulator
-  attr :console_interface
+  attr_reader :dice, :console_interface
 
   def initialize(dice: Dice.new, console_interface: nil)
+    @dice = dice
     @console_interface = console_interface
   end
 
   def run
     continue = true
     while continue do
+      console_interface.print_result("You rolled a #{dice.roll}.")
       console_interface.ask_question
       continue = console_interface.answer
     end
--- dice_exercise/test/dice_rolling_simulator_test.rb
+++ dice_exercise/test/dice_rolling_simulator_test.rb
@@ -12,18 +12,33 @@ class DiceRollingSimulatorTest < Minitest::Test
     should 'ask the user if they want to continue' do
       console_interface = ConsoleInterface.new
       console_interface.expects(:ask_question)
-      console_interface.stubs(:answer)
+      console_interface.stubs(:answer).returns(false)
+      console_interface.stubs(:print_result)
       
       simulator = DiceRollingSimulator.new(console_interface: console_interface)
       simulator.run
     end
 
-    should 'ask the user if they want to continue until they answer n' do
+    should 'ask the user if they want to continue until they answer no' do
       console_interface = ConsoleInterface.new
       console_interface.expects(:ask_question).times(3)
       console_interface.stubs(:answer).returns(true).returns(true).then.returns(false)
+      console_interface.stubs(:print_result)
       
       simulator = DiceRollingSimulator.new(console_interface: console_interface)
+      simulator.run
+    end
+
+    should 'print the dice results' do
+      dice = Dice.new
+      dice.stubs(:roll).returns(5)
+
+      console_interface = ConsoleInterface.new
+      console_interface.expects(:print_result).with("You rolled a 5.")
+      console_interface.stubs(:ask_question)
+      console_interface.stubs(:answer).returns(false)
+
+      simulator = DiceRollingSimulator.new(dice: dice, console_interface: console_interface)
       simulator.run
     end
   end
