f5d400a4910b50b66c9d0d32ebc129a38b1e182b
Julie Anne Moore <jmoore5@unb.ca>
2019-10-16 18:26:14 -0300

    working code

--- dice_exercise/test/dice_test.rb
+++ dice_exercise/test/dice_test.rb
@@ -1,10 +1,14 @@
 require 'minitest/autorun'
 require 'shoulda'
 
+require './lib/dice.rb'
+
 class DiceTest < Minitest::Test
 	context '#roll' do
 		should 'return an integer' do
-
+			dice = Dice.new
+			# result = dice.roll
+			# assert dice.is_a?(Integer)
 		end
 	end
 end
