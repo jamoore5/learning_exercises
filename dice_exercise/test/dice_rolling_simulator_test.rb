0a568718fd679dfee3f538617e27267421240a42
Julie Anne Moore <jmoore5@unb.ca>
2019-10-16 20:30:29 -0300

    working code

--- dice_exercise/test/dice_rolling_simulator_test.rb
+++ dice_exercise/test/dice_rolling_simulator_test.rb
@@ -41,5 +41,18 @@ class DiceRollingSimulatorTest < Minitest::Test
       simulator = DiceRollingSimulator.new(dice: dice, console_interface: console_interface)
       simulator.run
     end
+
+    should 'print the dice results same number of times the simulator continues' do
+      dice = Dice.new
+      dice.stubs(:roll).returns(5)
+
+      console_interface = ConsoleInterface.new
+      console_interface.expects(:print_result).with("You rolled a 5.").times(3)
+      console_interface.stubs(:ask_question).times(3)
+      console_interface.stubs(:answer).returns(true).returns(true).then.returns(false)
+
+      simulator = DiceRollingSimulator.new(dice: dice, console_interface: console_interface)
+      simulator.run
+    end
   end
 end
